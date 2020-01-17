1cd6d909d548a0ac1d3ce009687a98b4714a3035
okutamatanuki <seinoazumi@gmail.com>
2020-01-17 23:10:04 +0900

    refactoring single page

--- archive.php
+++ archive.php
@@ -21,7 +21,7 @@ get_header();
   <section class="archive">
     <div class="content__inner">
       <figure class="content__inner--l">
-        <img src="<?php echo get_template_directory_uri();?>/img/archve_keyviz.png" alt="対象者のイメージ" class="keyvis__img w-100">
+        <img src="<?php echo get_template_directory_uri();?>/img/archve_keyviz.png" alt="対象者のイメージ" class="keyvis__arc w-100">
       </figure>
 
       <div class="content__inner--r">
--- template-parts/top-other.php
+++ template-parts/top-other.php
@@ -1,15 +1,13 @@
 <section class="archive">
-  <div class="content__inner">
-    <figure class="content__inner--l">
-      <img src="<?php echo get_template_directory_uri();?>/img/archve_keyviz.png" alt="対象者のイメージ" class="keyvis__img w-100">
-    </figure>
+  <figure class="content__inner--l">
+    <img src="<?php echo get_template_directory_uri();?>/img/archve_keyviz.png" alt="対象者のイメージ" class="keyvis__img w-100">
+  </figure>
 
-    <div class="content__inner--r">
-      <div class="section">
-        <h2 class="section__title">
-          <?php the_title(); ?>
-        </h3>
-      </div>
+  <div class="content__inner--r">
+    <div class="section">
+      <h2 class="section__title">
+        <?php the_title(); ?>
+      </h3>
     </div>
   </div>
 </section>
