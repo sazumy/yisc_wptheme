bf731e1bd119007c120a675a82056608d714a10a
okutamatanuki <seinoazumi@gmail.com>
2020-01-18 20:31:11 +0900

    refactoring pages

--- page.php
+++ page.php
@@ -13,24 +13,25 @@
 get_header();
 ?>
 
+
+<!-- 固定ページのスラッグを$slugに代入 -->
+<?php $slug = esc_attr($post->post_name); ?>
+
+<section class="page-top <?php echo $slug; ?> container-fluid  py-4 d-flex justify-content-center">
+  <?php if(is_page(array('foryouth', 'forparents', 'steps'))){ 
+    // トップが２カラムのキービジュアルです。
+    get_template_part('template-parts/top-basic');
+  }  elseif(is_page(array('aboutus', 'schedule', 'reservation'))) {
+    // トップが１カラムのキービジュアルです。
+    get_template_part('template-parts/top-single');
+  } else {
+    // 汎用ページのキービジュアルです。
+    get_template_part('template-parts/top-other');
+  }
+  ?>
+</section>
+
 <main class="">
-  <!-- 固定ページのスラッグを$slugに代入 -->
-  <?php $slug = esc_attr($post->post_name); ?>
-
-  <section class="page-top <?php echo $slug; ?> container-fluid  py-4 d-flex justify-content-center">
-    <?php if(is_page(array('foryouth', 'forparents', 'steps'))){ 
-      // トップが２カラムのキービジュアルです。
-      get_template_part('template-parts/top-basic');
-    }  elseif(is_page(array('aboutus', 'schedule', 'reservation'))) {
-      // トップが１カラムのキービジュアルです。
-      get_template_part('template-parts/top-single');
-    } else {
-      // 汎用ページのキービジュアルです。
-      get_template_part('template-parts/top-other');
-    }
-    ?>
-  </section>
-
   <div class="container">
     <!-- start wordpress loop -->
     <?php
